91f321ed470b5876fe919b677f804d41dd2079e8
Luis Valdenêncio <ls04af@gmail.com>
2018-09-12 18:09:39 -0300

    now the student list is dynamic

--- public/js/main.js
+++ public/js/main.js
@@ -332,16 +332,195 @@ var brandBoxChart4 = new Chart($('#social-box-chart-4'), {
 /*****************************************************************
 All the javascript written bellows deals with dynamic manipulation
 ******************************************************************/
-var table = document.querySelector("table");
-for (var x = 0; Number(table.attributes[1]) > x; x++) {
-    table.children[1].appendChild();
+
+const objectToHTML = function(obj) {
+  const element = document.createElement(obj.tag)
+  if (obj.content) element.innerHTML = obj.content
+
+  if (obj.attr) {
+      for (const name in obj.attr) {
+      const value = obj.attr[name]
+      element.setAttribute(name, value)
+    }
+  }
+
+  if (obj.events) {
+    for (const name in obj.events) {
+      const listener = new Function(obj.events[name]).bind(element)
+      element.addEventListener(name, listener)
+    }
+  }
+
+  if (obj.style) {
+    for (const property in obj.style) {
+      const value = obj.style[property]
+      element.style[property] = value
+    }
+  }
+
+  if (obj.children) {
+      if (obj.children.length > 1) {
+        for (var x = 0; x < obj.children.length; x++) {
+          element.appendChild(objectToHTML(obj.children[x]));
+        }
+      } else element.appendChild(objectToHTML(obj.children));
+  }
+
+  return element
 }
 
-function createElement(tag, className, attribue) {
-
-}
-
-function reverseString(string) {
-    if (string.length == 1) return string.charAt(string.length-1);
-    return string.charAt(string.length-1) + reverseString(string.slice(string.length - 1));  // passo de recursão
+var table = document.querySelector("table"),
+    counter = 1,
+    name = "Luís Valdenêncio";
+for (var x = 0; Number(table.attributes[1].value) > x; x++) {
+    table.children[1].appendChild(objectToHTML(
+      {
+        tag: 'tr',
+        children : [
+          {
+          tag: 'td',
+          attr: {
+            class: "text-center"
+          },
+          children : {
+            tag : "div",
+            attr: {
+              class : "avatar"
+            },
+            children : [
+              {
+                tag : "img",
+                attr : {
+                  class : "img-avatar",
+                  src : ""
+                }
+              },
+              {
+                tag : "span",
+                attr : {
+                  class : "avatar-status badge-success"
+                }
+              }]
+            }
+          },
+          {
+            tag : 'td',
+            children : [
+              {
+                tag : "div",
+                content : `${counter} | ${name}`
+              },
+              {
+                tag : "div",
+                attr: {
+                  class : "small text-muted"
+                },
+                children : {
+                  tag : "span",
+                  content : "Nasce Em: Jan 1, 2007"
+                }
+              }
+            ]
+          },
+          {
+            tag : "td",
+            attr : {
+              class : "text-center"
+            },
+            children : {
+              tag : "i",
+              content : "",
+              attr : {
+                class : "flag-icon flag-icon-us h4 mb-0",
+                id : "us",
+                title : "us"
+              }
+            }
+          },
+          {
+            tag : "td",
+            children : [
+              {
+                tag : "div",
+                attr : {
+                  class : "clearfix"
+                },
+                children : [
+                  {
+                    tag : "div",
+                    attr : {
+                        class : "float-left"
+                    },
+                    children : {
+                      tag : "strong",
+                      content : "50%"
+                    }
+                  },
+                  {
+                    tag : "div",
+                    attr : {
+                      class : "float-right"
+                    },
+                    children : {
+                      tag : "small",
+                      content : "Jun 11, 2005 - Jul 10, 2015",
+                      attr : {
+                        class : "text-muted"
+                      }
+                    }
+                  },
+                ]
+              },
+              {
+                tag : "div",
+                attr : {
+                  class : "progress progress-xs"
+                },
+                children : {
+                  tag : "div",
+                  attr : {
+                    class : "progress-bar bg-success",
+                    role : "progressbar",
+                    style : "width : 50%",
+                    "aria-valuenow" : "50",
+                    "aria-valuemin" : "0",
+                    "aria-valuemax" : "100"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            tag : "td",
+            attr : {
+              class : "text-center"
+            },
+            children : {
+              tag : "i",
+              attr : {
+                class : "fa fa-cc-mastercard",
+                style : "font-size:24px"
+              }
+            }
+          },
+          {
+            tag : "td",
+            children : [
+              {
+                  tag : "div",
+                  content : "Última Aula",
+                  attr : {
+                    class : "small text-muted"
+                  }
+              },
+              {
+                tag : "string",
+                content : "10 Seg Atraz"
+              }
+            ]
+          }
+        ]
+      }
+    ));
+    counter++;
 }
